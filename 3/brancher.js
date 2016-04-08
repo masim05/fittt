6bac3d2118e8cd367916e4a41837a7792e7c8c77
Maxim Ryndin <masim_r@mail.ru>
2016-04-08 22:02:26 +0300

    3 Implement brancher without order.

--- 3/brancher.js
+++ 3/brancher.js
@@ -1,3 +1,35 @@
-module.exports = function (input) {
-  return input;
+module.exports = function (_input) {
+  var i, l;
+  var hash = {},
+  // Do not modify _input
+    input = JSON.parse(JSON.stringify(_input));
+
+  for (i = 0, l = input.length; i < l; i++) {
+    hash[input[i].id] = i;
+  }
+
+  var parent;
+  for (i = input.length; i-- > 0;) {
+    // Do nothing on top level nodes
+    if (!input[i].parentId) {
+      continue;
+    }
+
+    parent = input[hash[input[i].parentId]];
+    if (!parent.children) {
+      parent.children = [];
+    }
+    parent.children.push(input[i]);
+    input[i] = undefined;
+  }
+
+  // Prepare output
+  var output = [];
+  for (i = 0, l = input.length; i < l; i++) {
+    if (input[i]) {
+      output.push(input[i]);
+    }
+  }
+
+  return output;
 };
