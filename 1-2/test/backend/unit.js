7a5fe2ccef68daf4eca547f81e5f4fbda57ba525
Maxim Ryndin <masim_r@mail.ru>
2016-04-10 16:46:53 +0300

    1-2 Add orchestrator test.

--- 1-2/test/backend/unit.js
+++ 1-2/test/backend/unit.js
@@ -1,16 +1,32 @@
 var assert = require('assertthat');
 var Fetcher = require('../../components/fetcher');
+var orchestrator = require('../../components/orchestrator');
 
-describe('Fetcher', function () {
-  it('should fetch https://www.reddit.com/r/javascript/.json', function (done) {
+describe('Units', function () {
+  describe('Fetcher', function () {
+    it('should fetch https://www.reddit.com/r/javascript/.json', function (done) {
 
-    var fetcher = new Fetcher('https://www.reddit.com/r/javascript/.json');
-    fetcher.fetch(function (error, results) {
-      assert.that(error).is.falsy();
-      assert.that(results.response.statusCode).is.equalTo(200);
-      assert.that(results.body).is.not.falsy();
+      var fetcher = new Fetcher('https://www.reddit.com/r/javascript/.json');
+      fetcher.fetch(function (error, results) {
+        assert.that(error).is.falsy();
+        assert.that(results.response.statusCode).is.equalTo(200);
+        assert.that(results.body).is.not.falsy();
 
-      done();
+        done();
+      });
+    });
+  });
+
+  describe('Orchestrator', function () {
+    it('should perform the work somehow', function (done) {
+      this.timeout(8000);
+      var form = {
+        uri: 'https://www.reddit.com/r/javascript/.json'
+      };
+      orchestrator(form, function (error) {
+        //console.log(arguments);
+        done(error);
+      });
     });
   });
 });
