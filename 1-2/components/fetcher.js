44ac7170c1f796e9b3b6cb8a4558149f22d05897
Maxim Ryndin <masim_r@mail.ru>
2016-04-10 16:18:39 +0300

    1-2 Correct fetcher in a proper way.

--- 1-2/components/fetcher.js
+++ 1-2/components/fetcher.js
@@ -4,7 +4,16 @@ function Fetcher(_uri) {
   var uri = _uri;
 
   this.fetch = function (callback) {
-    request.get(uri, callback);
+    request.get(uri, {json: true}, function (error, response, body) {
+      return callback(
+        error,
+        {
+          response: response,
+          body: body
+        }
+      );
+
+    });
   };
 }
 
