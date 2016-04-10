483892fa974c1eed2b043b6593bf88b891298a3f
Maxim Ryndin <masim_r@mail.ru>
2016-04-10 14:16:32 +0300

    1-2 Use jade template framework.

--- 1-2/app.js
+++ 1-2/app.js
@@ -3,6 +3,10 @@ var path = require('path');
 
 var app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'public')));
+
 var routes = {
   index: require('./routes/index')
 };
--- 1-2/routes/index.js
+++ 1-2/routes/index.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-  res.sendStatus(200);
+  var page = {
+    title: 'FITTT-1-2',
+    message: 'Message'
+  };
+  res.render('index', page);
 });
 
 module.exports = router;
