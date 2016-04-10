1ca2f92351e19d3fd23d9fdd5581ddd8df28299e
Maxim Ryndin <masim_r@mail.ru>
2016-04-10 17:10:56 +0300

    1-2 Add test for selector.

--- 1-2/test/backend/unit.js
+++ 1-2/test/backend/unit.js
@@ -1,5 +1,10 @@
 var assert = require('assertthat');
 var Fetcher = require('../../components/fetcher');
+var Selector = require('../../components/selector');
+var Sorter = require('../../components/handlers/sorter');
+var Aggregator = require('../../components/handlers/aggregator');
+var Csv = require('../../components/renderers/csv');
+var Sql = require('../../components/renderers/sql');
 var orchestrator = require('../../components/orchestrator');
 
 describe('Units', function () {
@@ -11,6 +16,46 @@ describe('Units', function () {
         assert.that(error).is.falsy();
         assert.that(results.response.statusCode).is.equalTo(200);
         assert.that(results.body).is.not.falsy();
+
+        done();
+      });
+    });
+  });
+
+  describe('Selector', function () {
+    it('should select sotring with sql', function (done) {
+      var selector = new Selector();
+      var form = {
+        uri: 'https://www.reddit.com/r/javascript/.json',
+        operation: 'sorting',
+        format: 'sql'
+      };
+      selector.select(form, function (error, results) {
+        if (error) {
+          throw error;
+        }
+
+        assert.that(new results.handler.ctor() instanceof Sorter).is.true();
+        assert.that(new results.renderer.ctor() instanceof Sql).is.true();
+
+        done();
+      });
+    });
+
+    it('should select aggregation with csv', function (done) {
+      var selector = new Selector();
+      var form = {
+        uri: 'https://www.reddit.com/r/javascript/.json',
+        operation: 'aggregation',
+        format: 'csv'
+      };
+      selector.select(form, function (error, results) {
+        if (error) {
+          throw error;
+        }
+
+        assert.that(new results.handler.ctor() instanceof Aggregator).is.true();
+        assert.that(new results.renderer.ctor() instanceof Csv).is.true();
 
         done();
       });
