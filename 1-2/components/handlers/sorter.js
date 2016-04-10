5cb8d0cda339956f88a3175b51225e5ae96fd404
Maxim Ryndin <masim_r@mail.ru>
2016-04-10 22:41:15 +0300

    1-2 Implement sorter.

--- 1-2/components/handlers/sorter.js
+++ 1-2/components/handlers/sorter.js
@@ -1,6 +1,35 @@
-function Sorter() {
-  this.handle = function (data, callback) {
+var _ = require('lodash');
+
+function Sorter(options) {
+  this.handle = function (_data, callback) {
+    // do not modify input
+    var data = _.cloneDeep(_data);
+    data.sort(getComparator(options));
     return callback(undefined, data);
+  };
+}
+
+function getComparator(options) {
+  var asc = (options.order.toLowerCase() === 'asc');
+
+  return function (_a, _b) {
+    var a = _.get(_a, options.field);
+    var b = _.get(_b, options.field);
+
+    if ((typeof a === 'number') && (typeof b === 'number')) {
+      return asc ? a - b : b - a;
+    } else {
+      a = a.toString();
+      b = b.toString();
+
+      if (a < b) {
+        return asc ? -1 : 1;
+      } else if (a > b) {
+        return asc ? 1 : -1;
+      } else {
+        return 0;
+      }
+    }
   };
 }
 
