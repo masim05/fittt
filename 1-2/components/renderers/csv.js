b2fbb3d0d63eb25189808c192c55b1251aeb474e
Maxim Ryndin <masim_r@mail.ru>
2016-04-11 00:27:05 +0300

    1-2 Implement csv renderer.

--- 1-2/components/renderers/csv.js
+++ 1-2/components/renderers/csv.js
@@ -1,6 +1,28 @@
-function Csv() {
+var _ = require('lodash');
+
+const PROPERTIES = [
+  'data.id',
+  'data.score',
+  'data.title',
+  'data.created_utc'
+];
+
+function Csv(options) {
+  var delimiter = options.delimiter;
+
   this.render = function (data, callback) {
-    return callback(undefined, data);
+    var outs = [PROPERTIES.join(delimiter)];
+    var current;
+    data.forEach(function (e) {
+      current = [];
+      PROPERTIES.forEach(function (p) {
+        current.push(JSON.stringify(_.get(e, p) || ''));
+      });
+      outs.push(current.join(delimiter));
+    });
+
+    var result = outs.join('\n');
+    return callback(undefined, result);
   };
 }
 
