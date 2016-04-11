b043007dcf65a99d33fbd496808ebb1374348ea2
Maxim Ryndin <masim_r@mail.ru>
2016-04-11 09:58:00 +0300

    1-2 Implement sql renderer.

--- 1-2/components/renderers/sql.js
+++ 1-2/components/renderers/sql.js
@@ -1,6 +1,35 @@
-function Sql() {
+var squel = require('squel');
+var _ = require('lodash');
+var mapping = {
+  'data.id': 'id',
+  'data.score': 'score',
+  'data.title': 'title',
+  'data.created_utc': 'created_utc'
+};
+
+function Sql(options) {
+  var tablename = options.tablename;
+
   this.render = function (data, callback) {
-    return callback(undefined, data);
+    var output = [];
+
+    data.forEach(function (e) {
+      var query = squel.insert().into(tablename);
+
+      for (var k in mapping) {
+        var v = _.get(e, k);
+        if (!v) {
+          continue;
+        }
+
+        query.set(mapping[k], v);
+      }
+
+      output.push(query.toString());
+    });
+
+    var results = output.join(';\n') + (output.length ? ';' : '');
+    return callback(undefined, results);
   };
 }
 
