da2ce8473f54de215d3930fe361ee751d860c606
Maxim Ryndin <masim_r@mail.ru>
2016-04-11 10:48:49 +0300

    1-2 Add stub for aggregator.

--- 1-2/components/handlers/aggregator.js
+++ 1-2/components/handlers/aggregator.js
@@ -1,6 +1,24 @@
+var _ = require('lodash');
+
 function Arrgegator() {
   this.handle = function (data, callback) {
-    return callback(undefined, data);
+    var output = {};
+    data.reduce(function (previous, current) {
+      var r = output[current.domain];
+      if (!r) {
+        r = output[current.domain] = {
+          domain: current.domain,
+          articles: 0,
+          scores: 0
+        }
+      }
+      r.articles++;
+      r.scores += current.score;
+    });
+
+    var results = _.values(output);
+
+    return callback(undefined, results);
   };
 }
 
