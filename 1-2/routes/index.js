565a2412bce0849dc586fc2358de50dcebb3add6
Maxim Ryndin <masim_r@mail.ru>
2016-04-11 22:52:10 +0300

    1-2 Handle post /

--- 1-2/routes/index.js
+++ 1-2/routes/index.js
@@ -1,12 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var orchestrate = require('../components/orchestrator');
 
 router.get('/', function (req, res) {
   var page = {
-    title: 'FITTT-1-2',
-    message: 'Message'
+    html_title: 'FITTT-1-2',
+    title: 'Process reddit.com data',
+    message: 'Fill the form below:'
   };
   res.render('index', page);
+});
+
+router.post('/', function (req, res, next) {
+  orchestrate(req.body, function (error, results) {
+    if (error) {
+      return next(error);
+    }
+
+    var page = {
+      html_title: 'FITTT-1-2',
+      title: 'Process reddit.com data',
+      message: 'Fill the form below:',
+      results: {
+        message: 'Results:',
+        raw: results.replace(/\n/g, '<br/>')
+      }
+    };
+    res.render('index', page);
+  });
 });
 
 module.exports = router;
