f08d3444dc056dc999d999add3386c2eeaf72237
Maxim Ryndin <masim_r@mail.ru>
2016-04-11 21:39:26 +0300

    1-2 Add body-parser and morgan logger.

--- 1-2/app.js
+++ 1-2/app.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
 
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('combined'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 var routes = {
   index: require('./routes/index')
